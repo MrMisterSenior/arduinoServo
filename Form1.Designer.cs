20367870aaccdc8df1d1029f5ea665c999f01884
MrMisterSenior <vladimirus2002user@gmail.com>
2022-01-01 01:57:28 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace ArduinoLight
+{
+    partial class ABOBA
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.PortName = "COM3";
+            // 
+            // ABOBA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "ABOBA";
+            this.Text = "ABOBA";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ABOBA_FormClosed);
+            this.Load += new System.EventHandler(this.ABOBA_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ABOBA_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoLight
+{
+    public partial class ABOBA : Form
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        public ABOBA()
+        {
+            InitializeComponent();
+        }
+        private void ABOBA_Load(object sender, EventArgs e)
+        {
+            serialPort.Open();
+        }
+        private void ABOBA_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (stopwatch.ElapsedMilliseconds > 50)
+            {
+                serialPort.Write($"{180 - e.X / (Size.Width / 180)},{180 - e.Y / (Size.Height / 180)}");
+                stopwatch.Restart();
+            }
+        }
+
+        private void ABOBA_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort.Write("90,90");
+            serialPort.Close();
+        }
+    }
+}
